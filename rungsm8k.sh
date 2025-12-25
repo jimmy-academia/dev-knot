b0dca3d59b449a4b0caab25a437a7d8b0f466ef7
Jimmy Yeh <marrch30@gmail.com>
2025-12-25 11:59:57 -0600

    cleanup

--- rungsm8k.sh
+++
@@ -1,5 +0,0 @@
-python main.py --task gsm8k:5 --scheme dknot --worker_llm gpt-4
-python main.py --task gsm8k:4 --scheme dknot --worker_llm gpt-4
-python main.py --task gsm8k:3 --scheme dknot --worker_llm gpt-4
-python main.py --task gsm8k:2 --scheme dknot --worker_llm gpt-4
-python main.py --task gsm8k:1 --scheme dknot --worker_llm gpt-4
